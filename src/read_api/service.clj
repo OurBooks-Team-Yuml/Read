627789f0e325abe1846ea41a8951e44d5d010eed
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-06-01 14:52:27 +0200

    Remove unneded route

--- src/read_api/service.clj
+++ src/read_api/service.clj
@@ -4,12 +4,11 @@
             [io.pedestal.http.route :as route]
             [io.pedestal.http.route.definition :refer [defroutes]]
             [ns-tracker.core :refer [ns-tracker]]
-            [read-api.views.hello :as hello]
             [read-api.interceptors :as interceptors]))
 
 (def routes
-  (route/expand-routes
-    #{["/reads/read/:book-id" :post [interceptors/echo interceptors/db-interceptor interceptors/read-book] :route-name :read]}))
+  (route/expand-routes)
+  #{["/reads/read/:book-id" :post [interceptors/echo interceptors/db-interceptor interceptors/read-book] :route-name :read]})
 
 (def modified-namespaces (ns-tracker "src"))
 
